6a182a71024c6692bfccbbdb287aebf3b246a754
Riste Tegovski <ristetegovski@gmail.com>
2016-03-02 13:07:52 +0100

    t

---
+++ Model/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public int Years { get; set; }
+        public string LerningLanguage { get; set; }
+
+    }
+}
