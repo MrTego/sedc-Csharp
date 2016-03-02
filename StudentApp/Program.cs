27d3144178bc652ab849948081de615997c92072
Riste Tegovski <ristetegovski@gmail.com>
2016-03-02 13:05:32 +0100

    t

--- StudentApp/Program.cs
+++ StudentApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,9 @@ namespace StudentApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HELLO");
+            Student newStudent = new Student();
+
+
         }
     }
 }
